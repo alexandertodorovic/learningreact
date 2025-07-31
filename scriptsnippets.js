82852d03dc667963bab2ac346566998ae96b73b3
Alexander Todorovic <127629967+alexandertodorovic@users.noreply.github.com>
2025-07-31 08:31:27 +0000

    Initialize new Vite project

--- scriptsnippets.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-function Person(name) { this.name = name; }
-let ferdinand =  new Person("Ferdinand"); // Oops
-console.log(ferdinand);
--- src/Components/Slide.jsx
+++
@@ -1,9 +0,0 @@
-const Slide = ({image}) => {
-    return (
-        <div className="slide">
-            <img src={image?.url} alt={image?.alt} />
-        </div>
-    );
-}
-
-export default Slide;
